753c7ef16c7fbc117103e85331205eeeb08b9a27
Simon Robinson <simon@robinson.ac>
2024-07-30 09:07:31 +0100

    Match only one (case-insensitive) content-type header

--- plugins/SMTPDisableO365Reactions.py
+++ plugins/SMTPDisableO365Reactions.py
@@ -1,9 +1,9 @@
 """An example Email OAuth 2.0 Proxy SMTP plugin that adds the Outlook-specific email header `x-ms-reactions: disallow`
 that disables message reactions. An additional optional parameter 'domain_whitelist' takes an array of recipient
-domains (for example, your own internal domain(s)). If any of these domains are present in the recipient list, the
-ability to react to this message will be retained. Note that this functionality applies to all recipients - i.e., if a
-single whitelisted recipient is present, all recipients will be able to react to the message. For further details about
-disabling reactions, see the Microsoft article: https://techcommunity.microsoft.com/t5/outlook-blog/r/ba-p/3928103.
+domains (for example, ['your-domain.com', 'example.com']). If any of these domains are present in the recipient list,
+the ability to react to this message will be retained. Note that this functionality applies to all recipients - i.e.,
+if a single whitelisted recipient is present, all recipients will be able to react to the message. For further details
+about disabling reactions, see the Microsoft article: https://techcommunity.microsoft.com/t5/outlook/r/ba-p/3928103.
 """
 
 import enum
@@ -14,6 +14,9 @@ import plugins.BasePlugin
 SMTP_MAIL_FROM_MATCHER = re.compile(b'MAIL FROM:.+\r\n', flags=re.IGNORECASE)
 SMTP_RCPT_TO_MATCHER = re.compile(b'RCPT TO:.+\r\n', flags=re.IGNORECASE)
 SMTP_BODY_RECIPIENT_MATCHER = re.compile(b'(?:To|Cc):.+\r\n\r\n', flags=re.IGNORECASE)
+
+CONTENT_TYPE_HEADER = b'\r\nContent-Type: '
+CONTENT_TYPE_MATCHER = re.compile(CONTENT_TYPE_HEADER, flags=re.IGNORECASE)
 
 
 class SMTPDisableO365Reactions(plugins.BasePlugin.BasePlugin):
@@ -26,14 +29,14 @@ class SMTPDisableO365Reactions(plugins.BasePlugin.BasePlugin):
     def __init__(self, domain_whitelist=None):
         super().__init__()
         self.domain_whitelist = [d.encode('utf-8') for d in domain_whitelist] if domain_whitelist else []
-        self.sending_state, self.previous_line_ended, self.whitelist_domain, self.whitelist_domain_logged = self.reset()
+        self.sending_state, self.previous_line_ended, self.whitelist_domain, self.header_processed = self.reset()
 
     def reset(self):
         self.sending_state = self.STATE.NONE
         self.previous_line_ended = False
         self.whitelist_domain = None
-        self.whitelist_domain_logged = False
-        return self.sending_state, self.previous_line_ended, self.whitelist_domain, self.whitelist_domain_logged
+        self.header_processed = False
+        return self.sending_state, self.previous_line_ended, self.whitelist_domain, self.header_processed
 
     def receive_from_client(self, byte_data):
         # SMTP: https://tools.ietf.org/html/rfc2821
@@ -58,19 +61,17 @@ class SMTPDisableO365Reactions(plugins.BasePlugin.BasePlugin):
                 self.sending_state = self.STATE.DATA
 
         elif self.sending_state == self.STATE.DATA:  # message contents
-            if self.whitelist_domain:
-                if not self.whitelist_domain_logged:
-                    self.whitelist_domain_logged = True
+            if not self.header_processed:
+                self.header_processed = True
+
+                if self.whitelist_domain:
                     self.log_debug('Found whitelisted domain `%s`' % self.whitelist_domain.decode('utf-8'),
                                    '- skipping adding `x-ms-reactions: disallow` header to outgoing message')
-            else:
-                byte_data_lines = byte_data.split(b'\r\n')
-                for i, item in enumerate(byte_data_lines):
-                    if item.startswith(b'Content-Type: '):
-                        byte_data_lines[i] = b'x-ms-reactions: disallow\r\n' + item
-                        self.log_debug('Added `x-ms-reactions: disallow` header to outgoing message')
-                        break
-                byte_data = b'\r\n'.join(byte_data_lines)
+                else:
+                    byte_data_parts = CONTENT_TYPE_MATCHER.split(byte_data, maxsplit=1)
+                    byte_data_parts[0] += b'\r\nx-ms-reactions: disallow'
+                    byte_data = CONTENT_TYPE_HEADER.join(byte_data_parts)
+                    self.log_debug('Added `x-ms-reactions: disallow` header to outgoing message')
 
             if byte_data.endswith(b'\r\n.\r\n') or (self.previous_line_ended and byte_data == b'.\r\n'):  # end of email
                 self.reset()
